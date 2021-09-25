870dfdc8a1e37532b0982dbfda67a546f24b9e59
waldemarciupa <waldemarciupa90@gmail.com>
2021-09-25 12:52:23 +0200

    created connection to DB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`App running on ${port}`);
+app.use(cors());
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+try {
+  mongoose.connect(process.env.MONGO_DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('Successfully connected to DB');
+} catch (error) {
+  console.log(`Error while connecting DB!!!: ${error}`);
+}
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from homepage' });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}`);
 });
