f1d7a0e9a77030dcb87d251e7494d055e68902a2
waldemarciupa <waldemarciupa90@gmail.com>
2021-09-29 14:34:50 +0200

    Auth user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,6 +23,33 @@ module.exports = {
       .status(400)
       .json({ message: 'User with this email already exist' });
   },
+  async authUser(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res
+          .status(200)
+          .json({ message: 'Enter your email and password ' });
+      }
+
+      const user = await User.findOne({ email });
+
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const userResponse = {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+        };
+
+        return res.json(userResponse);
+      } else {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
   async getUserById(req, res) {
     const { id } = req.params;
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -16,6 +16,7 @@ router.route('/task/:id').delete(taskController.deleteTask);
 
 // User
 router.route('/user/register').post(userController.registerUser);
+router.route('/user/login').get(userController.authUser);
 router.route('/user/:id').get(userController.getUserById);
 
 module.exports = router;
