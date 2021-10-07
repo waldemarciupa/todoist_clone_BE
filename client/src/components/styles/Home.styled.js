213578fb1dd06e41d6adf978272c9072c12077e4
waldemarciupa <waldemarciupa90@gmail.com>
2021-10-07 08:35:12 +0200

    working on home page

---
+++ client/src/components/styles/Home.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ListBox = styled.div`
+  padding: 16px 55px 0;
+`;
+
+export const DateHeader = styled.h1`
+  font-size: 20px;
+`;
+
+export const DateToday = styled.span`
+  color: #808080;
+  font-size: 12px;
+  font-weight: 400;
+`;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import Header from '../components/Header';
+import {
+  ListBox,
+  DateHeader,
+  DateToday,
+} from '../components/styles/Home.styled';
 
 const Home = () => {
   const user_id = localStorage.getItem('user_id');
 
-  console.log(user_id);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [project, setProject] = useState('');
+  const [priority, setPriority] = useState('');
+  const [completed, setCompleted] = useState(false);
+
+  const date = new Date();
 
   return (
-    <div>
+    <>
       <Header />
-      <h1>Hello from Homepage</h1>
-    </div>
+      <ListBox>
+        <DateHeader>
+          Today <DateToday>{date.toDateString()}</DateToday>
+        </DateHeader>
+      </ListBox>
+    </>
   );
 };
 
