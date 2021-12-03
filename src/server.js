73508ccba1590dbf208d3995b2aa1c0863b8b9b5
waldemarciupa <waldemarciupa90@gmail.com>
2021-12-03 12:39:58 +0100

    prepare for deployment

--- src/server.js
+++ src/server.js
@@ -23,11 +23,11 @@ try {
 app.use(router);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html')
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
     );
   });
 }
