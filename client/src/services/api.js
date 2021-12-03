61c6752010c7c81ffb1da09fa179b8a76828ec6d
waldemarciupa <waldemarciupa90@gmail.com>
2021-12-03 14:25:56 +0100

    fixing bugs found during deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+if (process.env.NODE_ENV === 'production') {
+  const api = axios.create({
+    baseURL: 'https://my-todoist-clone.herokuapp.com',
+  });
+
+  return api;
+}
+
 const api = axios.create({
   baseURL: 'http://localhost:8000',
 });
