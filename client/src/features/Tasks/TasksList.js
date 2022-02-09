4754bfd5872d6746636ff59ec003f38fc9caa16a
waldemarciupa <waldemarciupa90@gmail.com>
2022-02-09 15:23:31 +0100

    created task item component

--- client/src/features/Tasks/TasksList.js
+++ client/src/features/Tasks/TasksList.js
@@ -2,31 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   fetchTasks,
-  editTask,
   deleteTask,
   selectTasksByProject,
-  selectTaskSingle,
   resetTaskMessage,
 } from './tasksSlice';
-import { AiOutlineDelete, AiOutlineCheck } from 'react-icons/ai';
 import {
   ListBox,
   DateHeader,
   DateToday,
   TasksList,
-  Task,
-  ButtonWrapper,
-  TaskButton,
-  TaskButtonOuter,
-  TaskButtonInner,
-  TaskContent,
-  TaskLink,
-  TaskActions,
-  TaskTitle,
-  TaskDescription,
-  BranchWrapper,
-  TaskProject,
-  TaskBottomWrapper,
 } from '../../components/styles/Home.styled';
 import ButtonAddTask from '../../components/ButtonAddTask';
 import Bicycle from '../../components/svg/Bicycle';
@@ -34,6 +18,7 @@ import Peace from '../../components/svg/Peace';
 import Paint from '../../components/svg/Paint';
 import EmptyState from '../../components/EmptyState';
 import TaskCreate from './TaskCreate';
+import TaskItem from './TaskItem';
 
 const TaskList = () => {
   const [addTaskVisible, setAddTaskVisible] = useState(false);
@@ -76,70 +61,11 @@ const TaskList = () => {
         {tasks.length
           ? tasks.map((task) => {
               return (
-                <Task data-id={task._id} key={task._id}>
-                  <ButtonWrapper>
-                    <TaskButton>
-                      <TaskButtonOuter
-                        title='Complete task'
-                        completed={task.completed}
-                        onClick={() => {
-                          dispatch(
-                            editTask({
-                              id: task._id,
-                              completed: !task.completed,
-                            })
-                          );
-                        }}
-                        color={task.priority}
-                      >
-                        <TaskButtonInner
-                          completed={task.completed}
-                          color={task.priority}
-                        >
-                          <AiOutlineCheck
-                            style={{ width: '9px', height: '9px' }}
-                          />
-                        </TaskButtonInner>
-                      </TaskButtonOuter>
-                    </TaskButton>
-                  </ButtonWrapper>
-                  <TaskContent>
-                    <TaskLink
-                      onClick={() => {
-                        dispatch(selectTaskSingle(task._id));
-                      }}
-                      to={`/task/${task._id}`}
-                    >
-                      <TaskTitle completed={task.completed}>
-                        {task.title}
-                      </TaskTitle>
-                      <TaskDescription>{task.description}</TaskDescription>
-                      <TaskBottomWrapper>
-                        {task.subtasks.length ? (
-                          <BranchWrapper>
-                            <img
-                              width='16px'
-                              height='16px'
-                              alt='branch'
-                              src='/images/branch.svg'
-                            />
-                            {
-                              task.subtasks.filter((task) => task.completed)
-                                .length
-                            }
-                            /{task.subtasks.length}
-                          </BranchWrapper>
-                        ) : (
-                          <div></div>
-                        )}
-                        <TaskProject>{task.project}</TaskProject>
-                      </TaskBottomWrapper>
-                    </TaskLink>
-                  </TaskContent>
-                  <TaskActions title='Delete' onClick={deleteTaskHandler}>
-                    <AiOutlineDelete style={{ color: '#202020' }} />
-                  </TaskActions>
-                </Task>
+                <TaskItem
+                  key={task._id}
+                  task={task}
+                  deleteTaskHandler={deleteTaskHandler}
+                />
               );
             })
           : null}
