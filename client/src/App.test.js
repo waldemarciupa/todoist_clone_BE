dabd070e9a30dff6d23e0ab5fc0f57ed84aa6080
waldemarciupa90@gmail.com <Waldemar Ciupa>
2023-01-16 14:10:07 +0100

    Move client files to seperate repository

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from './test-utils';
-import App from './App';
-
-test('renders sign up button', () => {
-  render(<App />, []);
-  const signUpBtn = screen.getByText('Sign up');
-  expect(signUpBtn).toBeInTheDocument();
-});
